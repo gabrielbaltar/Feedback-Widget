c8c2ce0895d3beea4b209baa7a7b1cab09956744
Gabriel Baltar <gabriel.baltar21@hotmail.com>
2022-05-03 22:41:02 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { Widget } from "./components/widget"
+
+// Exportando o componete para a página principal main, que está ligada ao único arquivo html
+export function App() {
+  return <Widget/>
+}
